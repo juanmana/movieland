7c99e2bcaa228dde6f4c405aa3b5f2506c1e12b3
juanmana <juanmana0402@gmail.com>
2019-04-24 18:57:38 +0200

    added movie local storage with tick verde

--- models/movie.js
+++ models/movie.js
@@ -14,7 +14,7 @@ const movieSchema = new Schema({
   Country:{type:String},
   Poster: {type:String},
   imbdRating: {type:String},
-  imbdID:{type:String},
+  imdbID:{type:String},
   Website:{type:String},
 
 
--- routes/movies.js
+++ routes/movies.js
@@ -1,25 +1,100 @@
-
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const User = require('../models/User');
+const Movie = require('../models/movie');
 
 router.get("/profile", (req, res, next) => {
+  console.log(req.user)
+
+  console.log(req.session.passport.user)
   res.render("movies/profile");
 });
 
 
 router.get("/info-movie/:imdbID", (req, res, next) => {
-  axios.get(`https://www.omdbapi.com/?i=${req.params.imdbID}&apikey=507e1127&type=movie`)
-  .then(movie => {
-    res.render("movies/info-movie", {movie})
-    // , want, watched
+  Movie
+    .find({
+      imbdID: req.params.imdbID
+    })
+    .then(foundMovie => {
+      if (foundMovie.length === 0) {
+        axios.get(`https://www.omdbapi.com/?i=${req.params.imdbID}&apikey=507e1127&type=movie`)
+          .then(movie => {
+            console.log(movie)
+            Movie
+              .create(movie.data)
+              .then(movieSaved => res.render("movies/info-movie", {
+                movie
+              }))
+            // , want, watched
+          })
+      } else {
+        res.render("movies/info-movie", {
+          foundMovie
+        })
+      }
+    })
+
+
+
+
+    .catch(error => console.log(error))
+})
+
+router.post('/want/:imdbID', (req, res) => {
+  //req.user.wantMovies.includes(req.params.id)
+  console.log(req.session.passport.user)
+
+  User.findByIdAndUpdate(req.user._id, {
+      $push: {
+        wantMovies: req.params.imdbID
+      }
+    }, {
+      new: true
+    })
+    .then((usermod) => {
+
+      res.json({
+        movieAssociated: true,
+        movieID: req.params.imdbID,
+        userID: req.user._id
+      })
+
+      //res.render("movies/want", {data})
+
+    })
+
+})
+
+router.get('/want', (req, res) => {
+
+  /* const wantedMovies = []
+  /* const wantedMovies = []
+  
+  req.user.wantMovies.forEach(oneMovie=>{
+
+    axios.get(`https://www.omdbapi.com/?i=${oneMovie}&apikey=507e1127&type=movie`)
+    .then(movie => {
+        console.log(movie.data.Title)
+        wantedMovies.push(movie.data.Title)
+    })
+  }) */
+  req.user.wantMovies.forEach(oneMovie=>{
+
+    axios.get(`https://www.omdbapi.com/?i=${oneMovie}&apikey=507e1127&type=movie`)
+    .then(movie => {
+        console.log(movie.data.Title)
+        wantedMovies.push(movie.data.Title)
+    })
+  })
+  data = req.user.wantMovies
+  res.render("movies/want", {
+    data
   })
 
 
-  .catch(error => console.log(error))
 })
-
-
 
 
 
